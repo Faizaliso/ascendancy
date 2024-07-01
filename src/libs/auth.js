231835b807287b3fe1250bf5fcfbdbc761d97ec4
Frengky Manjo <frengkysimanjorang29@gmail.com>
2024-07-01 15:58:06 +0700

    add dashboard

---
+++ src/libs/auth.js
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken"
+
+export function auth() {
+    const token = cookies().get("token")?.value;
+
+    if (!token) {
+        return null;
+    }
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        return payload;
+    } catch (error) {
+        return null;
+    }
+}
